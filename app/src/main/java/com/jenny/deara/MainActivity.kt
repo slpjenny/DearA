3660d7c8ff88fee60fe034cb86fb9f5006616eff
박지영 <slpjenny@naver.com>
2022-09-07 16:06:03 +0900

    .

---
+++ app/src/main/java/com/jenny/deara/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jenny.deara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//dd
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
