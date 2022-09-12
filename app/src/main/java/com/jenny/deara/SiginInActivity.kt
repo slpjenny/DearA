5df06e1f79da334622c4a1a34cc52b952e4f7277
박지영 <slpjenny@naver.com>
2022-09-12 17:18:55 +0900

    [Design]: activity_sigin_in.xml 디자인 초안

---
+++ app/src/main/java/com/jenny/deara/SiginInActivity.kt
@@ -0,0 +1,11 @@
+package com.jenny.deara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiginInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigin_in)
+    }
+}
