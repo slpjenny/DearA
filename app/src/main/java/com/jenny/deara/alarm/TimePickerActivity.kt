065bac6c613e141729e353cd70d84e39e635bc47
hyo <hyo2443@naver.com>
2022-09-14 15:40:08 +0900

    [DESIGN]:activity_alarm_main, activity_time_picker.xml 디자인 완료

---
+++ app/src/main/java/com/jenny/deara/alarm/TimePickerActivity.kt
@@ -0,0 +1,12 @@
+package com.jenny.deara.alarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jenny.deara.R
+
+class TimePickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_picker)
+    }
+}
