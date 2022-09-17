8298a8b9f3a94e440ee31ecfec5bff648b9f9a60
박지영 <slpjenny@naver.com>
2022-09-17 15:00:33 +0900

    [Feat]: 로그인 페이지 -> 비밀번호 찾기 페이지 이동 기능 추가

--- app/src/main/java/com/jenny/deara/LoginActivity.kt
+++ app/src/main/java/com/jenny/deara/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.jenny.deara.mypages.FindPwdActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,11 +13,20 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // '회원가입' textView 클릭시 SignInActivity 페이지로 이동
         val signInTxt = findViewById<TextView>(R.id.signInTxt)
         signInTxt.setOnClickListener {
-            val intent = Intent(this, MyPageActivity::class.java)
-            startActivity(intent)
+            val intentSignIn = Intent(this, SignInActivity::class.java)
+            startActivity(intentSignIn)
         }
+
+        // '비밀번호 찾기' textView 클릭시, FindPwdActivity로 이동
+        val findPwdTxt = findViewById<TextView>(R.id.findPwdTxt)
+        findPwdTxt.setOnClickListener {
+            val intentFindPwd = Intent(this, FindPwdActivity::class.java)
+            startActivity(intentFindPwd)
+        }
+
 
     }
 }
