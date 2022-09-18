5acaa0ed21fe7af4df64f19dd68e76818bd9c7fa
박지영 <slpjenny@naver.com>
2022-09-19 01:29:21 +0900

    [Feat]: 로그인 페이지,마이페이지 data binding 으로 페이지 연결

--- app/src/main/java/com/jenny/deara/LoginActivity.kt
+++ app/src/main/java/com/jenny/deara/LoginActivity.kt
@@ -4,29 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.jenny.deara.databinding.ActivityLoginBinding
 import com.jenny.deara.mypages.FindPwdActivity
 
 class LoginActivity : AppCompatActivity() {
 
+    val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
         // '회원가입' textView 클릭시 SignInActivity 페이지로 이동
-        val signInTxt = findViewById<TextView>(R.id.signInTxt)
-        signInTxt.setOnClickListener {
+        binding.signInTxt.setOnClickListener {
             val intentSignIn = Intent(this, SignInActivity::class.java)
             startActivity(intentSignIn)
+
         }
 
         // '비밀번호 찾기' textView 클릭시, FindPwdActivity로 이동
-        val findPwdTxt = findViewById<TextView>(R.id.findPwdTxt)
-        findPwdTxt.setOnClickListener {
+        binding.findPwdTxt.setOnClickListener {
             val intentFindPwd = Intent(this, FindPwdActivity::class.java)
             startActivity(intentFindPwd)
-        }
 
+        }
 
     }
 }
