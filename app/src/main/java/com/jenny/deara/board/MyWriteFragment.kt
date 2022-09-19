7583795b04ea87bfcbc4fbf157293e0d3db86f30
yein <inwon9892@naver.com>
2022-09-20 01:35:33 +0900

    [Design]: fragment_my_write.xml 파일 이름 오타 및 제목 수정

--- app/src/main/java/com/jenny/deara/board/MyWriteFragment.kt
+++ app/src/main/java/com/jenny/deara/board/MyWriteFragment.kt
@@ -1,10 +1,11 @@
-package com.jenny.deara
+package com.jenny.deara.board
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jenny.deara.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MyWirteFragment.newInstance] factory method to
+ * Use the [MyWriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MyWirteFragment : Fragment() {
+class MyWriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,12 +45,12 @@ class MyWirteFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MyWirteFragment.
+         * @return A new instance of fragment MyWriteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MyWirteFragment().apply {
+            MyWriteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
