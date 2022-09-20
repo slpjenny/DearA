a2fd1898c411ca3e0826e0322891058bdefcc621
yein <inwon9892@naver.com>
2022-09-21 00:23:39 +0900

    [Design]: activity_board_write.xml 완성

---
+++ app/src/main/java/com/jenny/deara/board/BoardWriteActivity.kt
@@ -0,0 +1,11 @@
+package com.jenny.deara.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+    }
+}
