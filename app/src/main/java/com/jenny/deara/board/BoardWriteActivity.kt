da88578fd8e4cf0b20eba6c4e2f5b0071e6a9fde
hyo <hyo2443@naver.com>
2022-09-21 15:36:30 +0900

    [DESIGN]:activity_time_picker.xml 디자인 수정

--- app/src/main/java/com/jenny/deara/board/BoardWriteActivity.kt
+++ app/src/main/java/com/jenny/deara/board/BoardWriteActivity.kt
@@ -2,6 +2,7 @@ package com.jenny.deara.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jenny.deara.R
 
 class BoardWriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
