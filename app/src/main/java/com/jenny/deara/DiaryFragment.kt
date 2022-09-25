28e3caf7053c02c96d2618733466680aa85d46f8
unknown <glgkwls1@naver.com>
2022-09-26 00:59:02 +0900

    [feat]: diary date picker dialog

--- app/src/main/java/com/jenny/deara/DiaryFragment.kt
+++ app/src/main/java/com/jenny/deara/DiaryFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import com.jenny.deara.databinding.FragmentDiaryBinding
+import com.jenny.deara.diary.DatePickerFragment
 import com.jenny.deara.diary.DiaryEditActivity
 
 class DiaryFragment : Fragment() {
@@ -24,9 +26,15 @@ class DiaryFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_diary, container, false)
 
+        //일기 작성하기
         binding.diaryplusBtn.setOnClickListener{
             val intent = Intent(context, DiaryEditActivity::class.java)
             startActivity(intent)
+        }
+
+        //날짜 선택하기
+        binding.datapicker.setOnClickListener {
+            DatePickerFragment().show(childFragmentManager, "SampleDialog")
         }
 
         return binding.root
---
+++ app/src/main/java/com/jenny/deara/diary/DatePickerFragment.kt
@@ -0,0 +1,35 @@
+package com.jenny.deara.diary
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.adapters.DatePickerBindingAdapter
+import androidx.fragment.app.DialogFragment
+import com.jenny.deara.R
+import com.jenny.deara.databinding.FragmentDatePickerBinding
+
+class DatePickerFragment : DialogFragment() {
+
+    private lateinit var binding: FragmentDatePickerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //false로 설정해 주면 화면밖 혹은 뒤로가기 버튼시 다이얼로그라 dismiss 되지 않는다.
+        isCancelable = true
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       binding = FragmentDatePickerBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
+
+}
