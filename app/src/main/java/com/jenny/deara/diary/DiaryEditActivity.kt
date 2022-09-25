9a11d33f55751cb102b6c4ecaa92656ddb7b0dd8
unknown <glgkwls1@naver.com>
2022-09-25 23:05:48 +0900

    [feat]: diary edit 연결

--- app/src/main/java/com/jenny/deara/diary/DiaryEditActivity.kt
+++ app/src/main/java/com/jenny/deara/diary/DiaryEditActivity.kt
@@ -2,17 +2,18 @@ package com.jenny.deara.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.jenny.deara.R
 
-class DiaryDetailVer1Activity : AppCompatActivity() {
-
+class DiaryEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diary_detail_ver1)
+        setContentView(R.layout.activity_diary_edit)
 
     }
 }
---
+++ app/src/main/java/com/jenny/deara/diary/DiaryEditVer2Fragment.kt
@@ -0,0 +1,33 @@
+package com.jenny.deara.diary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.jenny.deara.R
+import com.jenny.deara.databinding.FragmentDiaryEditVer2Binding
+
+class DiaryEditVer2Fragment : Fragment() {
+
+    private lateinit var binding: FragmentDiaryEditVer2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_diary_edit_ver2, container, false)
+
+        binding.toggleBtn2.setOnClickListener {
+            it.findNavController().navigate(R.id.action_diaryEditVer2Fragment_to_diaryEditVer1Fragment)
+        }
+        return binding.root
+    }
+
+}
