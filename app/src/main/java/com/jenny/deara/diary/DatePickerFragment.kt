ab85bd9af7f4fb1843866e96109a0aab11d9b4ee
unknown <glgkwls1@naver.com>
2022-09-26 01:19:31 +0900

    [design]:fragment_diary.xml design 수ëë정

--- app/src/main/java/com/jenny/deara/diary/DatePickerFragment.kt
+++ app/src/main/java/com/jenny/deara/diary/DatePickerFragment.kt
@@ -18,6 +18,7 @@ class DatePickerFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //false로 설정해 주면 화면밖 혹은 뒤로가기 버튼시 다이얼로그라 dismiss 되지 않는다.
         isCancelable = true
     }
