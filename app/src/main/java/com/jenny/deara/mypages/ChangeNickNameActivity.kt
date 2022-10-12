ae49fccc18553a717b6b3a14b9c5e766ac4693bd
박지영 <slpjenny@naver.com>
2022-10-12 17:33:01 +0900

    [feat]:닉네임 변경 페이지에 닉네임 뜨기

--- app/src/main/java/com/jenny/deara/mypages/ChangeNickNameActivity.kt
+++ app/src/main/java/com/jenny/deara/mypages/ChangeNickNameActivity.kt
@@ -2,11 +2,45 @@ package com.jenny.deara.mypages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.jenny.deara.R
+import com.jenny.deara.databinding.ActivityChangeNickNameBinding
+import com.jenny.deara.databinding.ActivityMyPageBinding
 
 class ChangeNickNameActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityChangeNickNameBinding.inflate(layoutInflater) }
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_nick_name)
+        setContentView(binding.root)
+
+        // Auth 초기화
+        auth = Firebase.auth
+
+        // 실시간 database reference 불러오기
+        database = Firebase.database.reference
+
+        // 현재 로그인 되어있는 정보 불러오기
+        val user = auth.currentUser
+
+        // firebase 에서 닉네임 불러오기
+        if(user!=null) {
+            database.child("users").child(user.uid).get().addOnSuccessListener {
+                binding.myNick.setText(it.value.toString())
+
+            }.addOnFailureListener {
+                binding.myNick.setText("닉네임 정보 없음")
+           }
+
+        }
     }
 }
