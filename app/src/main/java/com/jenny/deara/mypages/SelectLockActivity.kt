b37ace72b54c57d332a3ef6e09e801cdb499104c
hyo <hyo2443@naver.com>
2022-10-25 16:19:47 +0900

    [REFACTOR}:SelectLockActivity 코드 오류 수정

--- app/src/main/java/com/jenny/deara/mypages/SelectLockActivity.kt
+++ app/src/main/java/com/jenny/deara/mypages/SelectLockActivity.kt
@@ -1,11 +1,13 @@
 package com.jenny.deara.mypages
 
-import android.app.Activity
-import android.app.Dialog
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.jenny.deara.MyPageActivity
 import com.jenny.deara.R
 import com.jenny.deara.databinding.ActivitySignInBinding
@@ -14,22 +16,23 @@ import kotlinx.android.synthetic.main.activity_select_lock.*
 
 class SelectLockActivity : AppCompatActivity() {
 
-//    val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+
+    val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_lock)
 
         textView.text = if (switchButton.isChecked )
-            "암호/지문 잠금 끄기" else "암호/지문 잠금 켜기"
+            "암호/지문 잠금" else "암호/지문 잠금"
 
 
         switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                textView.text = "암호/지문 잠금 끄기"
+                textView.text = "암호/지문 잠금"
                 switchButton?.isSelected = switchButton?.isSelected != true
             } else {
-                textView.text = "암호/지문 잠금 켜기"
+                textView.text = "암호/지문 잠금"
                 switchButton?.isSelected = switchButton?.isSelected != false
             }
         }
@@ -38,7 +41,7 @@ class SelectLockActivity : AppCompatActivity() {
         //btnChangePwd는 switchButton이 isChecked된 상태에만 활성화
         //btnChangePwd 클릭 시 지문인식 또는 이전 암호 확인 후 변경 가능
 
-        //바인딩 오류 해결하기
+
         back.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
