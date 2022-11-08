cb6d08edc1baec6487cfa8b2e2809c9530925a79
yein <inwon9892@naver.com>
2022-11-08 16:24:23 +0900

    [feat]: 알람 기능 구현

--- app/src/main/java/com/jenny/deara/mypages/ChangeNickNameActivity.kt
+++ app/src/main/java/com/jenny/deara/mypages/ChangeNickNameActivity.kt
@@ -13,13 +13,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.jenny.deara.LoginActivity
 import com.jenny.deara.R
 import com.jenny.deara.databinding.ActivityChangeNickNameBinding
 import com.jenny.deara.databinding.ActivityMyPageBinding
@@ -51,18 +47,18 @@ class ChangeNickNameActivity : AppCompatActivity() {
         val user = auth.currentUser
 
         // firebase 에서 닉네임 불러오기
-        if(user!=null) {
+        if (user != null) {
             database.child("users").child(user.uid).get().addOnSuccessListener {
                 binding.myNick.setText(it.value.toString())
 
-
+                // 동기 방식으로 흐름 바꾸기!
                 // 닉네임 처음에 -> pbl 화이팅 하얗게 해서 안보이게했다가
                 // 내 닉네임 불러와지면 text 색상도 바뀌게 하기
                 binding.myNick.setTextColor(Color.BLACK)
 
             }.addOnFailureListener {
                 binding.myNick.setText("닉네임 정보 없음")
-           }
+            }
         }
 
 
@@ -71,37 +67,36 @@ class ChangeNickNameActivity : AppCompatActivity() {
             var nick = binding.editNickTxt.text.toString()
 
             // 닉네임 입력창 null 체크
-            if(nick.isNullOrEmpty()){
-                Toast.makeText(baseContext,"닉네임을 먼저 입력해주세요",Toast.LENGTH_SHORT).show()
-            }else{
+            if (nick.isNullOrEmpty()) {
+                Toast.makeText(baseContext, "닉네임을 먼저 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
 
                 // 닉네임이 작성되어야 중복 확인 기능 작동
                 database.addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
 
-                        // child 내에 있는 데이터만큼 반복합니다.
-                        // 회원가입에서는 제대로 중복확인 기능 돌아가는데 왜 여기서 똑같은 코드인데 안돌아가는거냐???? (진짜 모든 데이터가 다나오는데 이게머냐?)
-                        // 심지어 닉네임 똑같은걸로 저장도 되어버림..
-                        for (data in dataSnapshot.children) {
+                        var data = mapOf<String,String>()
 
-                            Log.d("nicklist", data.value.toString())
+                        // 이렇게 하면 users 밑의 uid와 닉네임들만 출력은 가능하다.
+//                        Log.d("nicklog",dataSnapshot.child("users").value.toString())
 
-                            if (nick == data.value.toString()){
-                                nickFor = 1
-                                Toast.makeText(baseContext,"다른 닉네임을 사용해주세요",Toast.LENGTH_SHORT).show()
-                                break
-                            }
-                        }
+                        data  = dataSnapshot.child("users").value as Map<String, String>
 
-                        // 닉네임 중복 안되었을 때
-                        if (nickFor == 0){
-                            Toast.makeText(baseContext,"사용하실 수 있는 닉네임입니다.",Toast.LENGTH_SHORT).show()
+                        // 와 이제 이렇게 하면 닉네임들만 뜬다.
+                        Log.d("nicklog", data.values.toString())
+
+                        if (nick in data.values){
+                            Toast.makeText(baseContext, "다른 닉네임을 사용해주세요.", Toast.LENGTH_SHORT).show()
+
+                        } else{
+                            Toast.makeText(baseContext, "사용하실 수 있는 닉네임입니다.", Toast.LENGTH_SHORT).show()
 
                             // 닉네임 중복 확인 통과 체크
                             checkNickBtn = 1
                         }
 
-                   }
+                    }
+
                     override fun onCancelled(databaseError: DatabaseError) {}
                 })
             }
@@ -114,32 +109,37 @@ class ChangeNickNameActivity : AppCompatActivity() {
 
 
             // 닉네임 입력창 null 체크
-            if(nick.isNullOrEmpty()){
-                Toast.makeText(baseContext,"닉네임을 먼저 입력해주세요",Toast.LENGTH_SHORT).show()
-            }else{
+            if (nick.isNullOrEmpty()) {
+                Toast.makeText(baseContext, "닉네임을 먼저 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else {
                 // 닉네임 중복 확인 여부 체크하기
-                if(checkNickBtn == 0){
-                    Toast.makeText(baseContext,"닉네임 중복 확인을 해주세요",Toast.LENGTH_SHORT).show()
-                }else{
+                if (checkNickBtn == 0) {
+                    Toast.makeText(baseContext, "닉네임 중복 확인을 해주세요", Toast.LENGTH_SHORT).show()
+                } else {
 
                     // 사용자의 uid 에 따라 닉네임을 저장
                     if (user != null) {
                         database.child("users").child(user.uid).setValue(nick)
 
-                        Toast.makeText(baseContext,"닉네임 변경이 완료되었습니다",Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, "닉네임 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
 
-                    }else{
-                        Toast.makeText(baseContext,"닉네임 정보 불러오기 오류", Toast.LENGTH_SHORT).show()
+                        // 다시 중복체크 변수 초기화
+                        checkNickBtn = 0
+                        onRestart()
+
+                    } else {
+                        Toast.makeText(baseContext, "닉네임 정보 불러오기 오류", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
 
-            }.addOnFailureListener {
-                binding.myNick.setText("닉네임 정보 없음")
-           }
-
-        }
-
     }
+
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+
 }
