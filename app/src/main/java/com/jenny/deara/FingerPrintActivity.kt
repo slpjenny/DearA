ef7959d672f272b34793d55261336300ca144803
hyo <hyo2443@naver.com>
2022-11-08 17:03:20 +0900

    [FEAT}:FingerpirntActivity 코드 수정

---
+++ app/src/main/java/com/jenny/deara/FingerPrintActivity.kt
@@ -0,0 +1,175 @@
+package com.jenny.deara
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import com.jenny.deara.databinding.ActivityFingerPrintBinding
+import com.jenny.deara.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_finger_print.*
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.concurrent.Executor
+
+class FingerPrintActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "FingerPrintActivity"
+    }
+
+    lateinit var binding: ActivityFingerPrintBinding
+
+    private var executor: Executor? = null
+    private var biometricPrompt: BiometricPrompt? = null
+    private var promptInfo: BiometricPrompt.PromptInfo? = null
+
+    private val loginLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Log.d(TAG, "registerForActivityResult - result : $result")
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d(TAG, "registerForActivityResult - RESULT_OK")
+                authenticateToEncrypt()  //생체 인증 가능 여부확인 다시 호출
+            } else {
+                Log.d(TAG, "registerForActivityResult - NOT RESULT_OK")
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        imageView.setColorFilter(Color.parseColor("#414E7D"), PorterDuff.Mode.SRC_IN);
+
+        //        setContentView(R.layout.activity_main)
+        binding = ActivityFingerPrintBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        biometricPrompt = setBiometricPrompt()
+        promptInfo = setPromptInfo()
+
+        //지문 인증 호출 버튼 클릭 시
+        binding.btnBioSet.setOnClickListener {
+            authenticateToEncrypt()  //생체 인증 가능 여부확인
+        }
+    }
+    private fun setPromptInfo(): BiometricPrompt.PromptInfo {
+
+        val promptBuilder: BiometricPrompt.PromptInfo.Builder = BiometricPrompt.PromptInfo.Builder()
+
+        promptBuilder.setTitle("앱 실행을 위한 지문 인식")
+        promptBuilder.setSubtitle("앱에 접근하려면 센서에 지문을 인식해 주세요.")
+        promptBuilder.setNegativeButtonText("취소")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) { //  안면인식 ap사용 android 11부터 지원
+            promptBuilder.setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+        }
+
+        promptInfo = promptBuilder.build()
+        return promptInfo as BiometricPrompt.PromptInfo
+    }
+
+
+    private fun setBiometricPrompt(): BiometricPrompt {
+        executor = ContextCompat.getMainExecutor(this)
+
+        biometricPrompt = BiometricPrompt(this@FingerPrintActivity, executor!!, object : BiometricPrompt.AuthenticationCallback() {
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@FingerPrintActivity, """"지문 인식 ERROR [ errorCode: $errorCode, errString: $errString ]""".trimIndent(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(this@FingerPrintActivity, "지문 인식 성공", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(this@FingerPrintActivity, "지문 인식 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        } )
+        return biometricPrompt as BiometricPrompt
+    }
+
+
+    /*
+    * 생체 인식 인증을 사용할 수 있는지 확인
+    * */
+    fun authenticateToEncrypt() = with(binding) {
+
+        Log.d(TAG, "authenticateToEncrypt() ")
+
+        var textStatus = ""
+        val biometricManager = BiometricManager.from(this@FingerPrintActivity)
+//        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+
+            //생체 인증 가능
+            BiometricManager.BIOMETRIC_SUCCESS -> textStatus = "지문 인식을 통해 앱에 접근할 수 있습니다."
+
+            //기기에서 생체 인증을 지원하지 않는 경우
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> textStatus = "이 디바이스는 생체 인증 기능을 지원하지 않습니다."
+
+            //현재 생체 인증을 사용할 수 없는 경우
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> textStatus = "현재 생체 인증을 사용할 수 없습니다."
+
+            //생체 인식 정보가 등록되어 있지 않은 경우
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                textStatus = "설정->보안에서 지문을 1개 이상 등록 후 다시 실행해 주세요."
+
+                val dialogBuilder = AlertDialog.Builder(this@FingerPrintActivity)
+                dialogBuilder
+                    .setTitle("나의 앱")
+                    .setMessage("지문 등록이 필요합니다.설정->보안에서 지문을 1개 이상 등록해 주세요.")
+//                    .setPositiveButton("확인") { dialog, which -> goBiometricSettings() }
+                    .setNegativeButton("확인") {dialog, which -> dialog.cancel() }
+                dialogBuilder.show()
+            }
+
+            //기타 실패
+            else ->  textStatus = "실패"
+
+        }
+        binding.tvStatus.text = textStatus
+
+        //인증 실행하기
+        goAuthenticate()
+    }
+
+
+    /*
+    * 생체 인식 인증 실행
+    * */
+    private fun goAuthenticate() {
+        Log.d(TAG, "goAuthenticate - promptInfo : $promptInfo")
+        promptInfo?.let {
+            biometricPrompt?.authenticate(it);  //인증 실행
+        }
+    }
+
+
+    /*
+    * 지문 등록 화면으로 이동
+    * */
+    fun goBiometricSettings() {
+        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            )
+        }
+        loginLauncher.launch(enrollIntent)
+    }
+}
