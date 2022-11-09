fb8c195909325dd7132456310cf1451cd9c8e3e4
hyo <hyo2443@naver.com>
2022-11-09 15:35:45 +0900

    [FEAT}:SelevtLockActivity 코드 수정

--- app/src/main/java/com/jenny/deara/mypages/SelectLockActivity.kt
+++ app/src/main/java/com/jenny/deara/mypages/SelectLockActivity.kt
@@ -23,24 +23,19 @@ class SelectLockActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_lock)
 
-        textView.text = if (switchButton.isChecked )
-            "암호/지문 잠금" else "암호/지문 잠금"
-
-
-        switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                textView.text = "암호/지문 잠금"
-                switchButton?.isSelected = switchButton?.isSelected != true
-            } else {
-                textView.text = "암호/지문 잠금"
-                switchButton?.isSelected = switchButton?.isSelected != false
-            }
-        }
-
-        //switchButton이 isChecked 된 경우 암호 설정+지문인식On
-        //btnChangePwd는 switchButton이 isChecked된 상태에만 활성화
-        //btnChangePwd 클릭 시 지문인식 또는 이전 암호 확인 후 변경 가능
-
+//        textView.text = if (switchButton.isChecked )
+//            "암호/지문 잠금" else "암호/지문 잠금"
+//
+//
+//        switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//                textView.text = "암호/지문 잠금"
+//                switchButton?.isSelected = switchButton?.isSelected != true
+//            } else {
+//                textView.text = "암호/지문 잠금"
+//                switchButton?.isSelected = switchButton?.isSelected != false
+//            }
+//        }
 
         back.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
