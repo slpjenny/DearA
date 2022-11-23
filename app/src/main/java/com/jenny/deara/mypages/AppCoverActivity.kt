7055bffce91173852342c95e4659895f4548752d
hyo <hyo2443@naver.com>
2022-11-23 17:31:04 +0900

    [DESIGN}:앱 로고 색상 변경 기능 추가

---
+++ app/src/main/java/com/jenny/deara/mypages/AppCoverActivity.kt
@@ -0,0 +1,55 @@
+package com.jenny.deara.mypages
+
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.jenny.deara.MyPageActivity
+import com.jenny.deara.R
+import com.jenny.deara.databinding.ActivitySignInBinding
+import kotlinx.android.synthetic.main.activity_app_cover.*
+import kotlinx.android.synthetic.main.activity_select_lock.*
+import kotlinx.android.synthetic.main.activity_select_lock.back
+
+class AppCoverActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_cover)
+
+        back.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        button1.setOnClickListener{
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+            )
+
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+            )
+            Toast.makeText(this@AppCoverActivity, "선택한 색상으로 변경됨", Toast.LENGTH_SHORT).show()
+        }
+
+        button2.setOnClickListener{
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias1"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+            )
+
+            packageManager?.setComponentEnabledSetting(
+                ComponentName(applicationContext.packageName, applicationContext.packageName + ".MainActivityAlias2"),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+            )
+            Toast.makeText(this@AppCoverActivity, "선택한 색상으로 변경됨", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
